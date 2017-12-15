f215705d5f7f052dc7f8c25b75685acf8420df2b
panqiming <497555328@qq.com>
2017-12-15 09:43:07 +0800

    +新闻app

---
+++ lib_news/src/main/java/com/znz/compass/znzlibray/bean/BaseCheckBean.java
@@ -0,0 +1,19 @@
+package com.znz.compass.znzlibray.bean;
+
+/**
+ * Date： 2017/10/27 2017
+ * User： PSuiyi
+ * Description：
+ */
+
+public class BaseCheckBean extends BaseZnzBean {
+    boolean isChecked;
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+}
