623cde3c1bbad841126002b686588f2e951e9b6f
panqiming <497555328@qq.com>
2017-12-18 16:45:20 +0800

    +新闻app

--- app/src/main/java/com/znz/news/ui/picture/PictureDetailAct.java
+++ app/src/main/java/com/znz/news/ui/picture/PictureDetailAct.java
@@ -1,7 +1,20 @@
 package com.znz.news.ui.picture;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.znz.compass.znzlibray.views.ZnzRemind;
+import com.znz.compass.znzlibray.views.ZnzToolBar;
 import com.znz.news.R;
 import com.znz.news.base.BaseAppActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Date： 2017/12/18 2017
@@ -10,6 +23,21 @@ import com.znz.news.base.BaseAppActivity;
  */
 
 public class PictureDetailAct extends BaseAppActivity {
+    @Bind(R.id.znzToolBar)
+    ZnzToolBar znzToolBar;
+    @Bind(R.id.znzRemind)
+    ZnzRemind znzRemind;
+    @Bind(R.id.llNetworkStatus)
+    LinearLayout llNetworkStatus;
+    @Bind(R.id.ivBack)
+    ImageView ivBack;
+    @Bind(R.id.tvComment)
+    TextView tvComment;
+    @Bind(R.id.flComment)
+    FrameLayout flComment;
+    @Bind(R.id.ivFav)
+    ImageView ivFav;
+
     @Override
     protected int[] getLayoutResource() {
         return new int[]{R.layout.act_picture_detail};
@@ -33,5 +61,29 @@ public class PictureDetailAct extends BaseAppActivity {
     @Override
     protected void loadDataFromServer() {
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.ivBack, R.id.tvComment, R.id.flComment, R.id.ivFav})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ivBack:
+                finish();
+                break;
+            case R.id.tvComment:
+
+                break;
+            case R.id.flComment:
+                gotoActivity(CommentListAct.class);
+                break;
+            case R.id.ivFav:
+                break;
+        }
     }
 }
