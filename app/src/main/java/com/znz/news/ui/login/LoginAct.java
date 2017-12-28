769c96f8edc221b9703bf30488dd9e48b43a9c79
panqiming <497555328@qq.com>
2017-12-28 15:43:29 +0800

    +新闻app

--- app/src/main/java/com/znz/news/ui/login/LoginAct.java
+++ app/src/main/java/com/znz/news/ui/login/LoginAct.java
@@ -1,11 +1,14 @@
 package com.znz.news.ui.login;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSONObject;
 import com.znz.compass.znzlibray.common.ZnzConstants;
+import com.znz.compass.znzlibray.network.znzhttp.ZnzHttpListener;
 import com.znz.compass.znzlibray.utils.StringUtil;
 import com.znz.compass.znzlibray.views.EditTextWithDel;
 import com.znz.compass.znzlibray.views.ZnzRemind;
@@ -13,6 +16,9 @@ import com.znz.compass.znzlibray.views.ZnzToolBar;
 import com.znz.news.R;
 import com.znz.news.base.BaseAppActivity;
 import com.znz.news.ui.TabHomeAct;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -43,6 +49,9 @@ public class LoginAct extends BaseAppActivity {
     TextView tvRegister;
     @Bind(R.id.tvForget)
     TextView tvForget;
+
+    private CountDownTimer timer;
+    private boolean isClickCode;
 
     @Override
     protected int[] getLayoutResource() {
@@ -85,6 +94,42 @@ public class LoginAct extends BaseAppActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tvCode:
+                if (StringUtil.isBlank(mDataManager.getValueFromView(etUserName))) {
+                    mDataManager.showToast("请输入手机号");
+                    return;
+                }
+                if (!StringUtil.isMobile(mDataManager.getValueFromView(etUserName))) {
+                    mDataManager.showToast("请输入正确的手机号");
+                    return;
+                }
+
+                isClickCode = true;
+
+                Map<String, String> params = new HashMap<>();
+                params.put("mobile", mDataManager.getValueFromView(etUserName));
+                mModel.requestCode(params, new ZnzHttpListener() {
+                    @Override
+                    public void onSuccess(JSONObject response) {
+                        super.onSuccess(response);
+                        timer = new CountDownTimer(60000, 1000) {
+                            public void onTick(long millisUntilFinished) {
+                                tvCode.setClickable(false);
+                                tvCode.setText(millisUntilFinished / 1000 + "秒");
+                            }
+
+                            public void onFinish() {
+                                tvCode.setText("重新发送");
+                                tvCode.setClickable(true);
+                            }
+                        };
+                        timer.start();
+                    }
+
+                    @Override
+                    public void onFail(String error) {
+                        super.onFail(error);
+                    }
+                });
                 break;
             case R.id.tvLogin:
                 if (StringUtil.isBlank(mDataManager.getValueFromView(etUserName))) {
@@ -96,7 +141,11 @@ public class LoginAct extends BaseAppActivity {
                     return;
                 }
                 if (StringUtil.isBlank(mDataManager.getValueFromView(etPsd))) {
-                    mDataManager.showToast("请输入密码");
+                    mDataManager.showToast("请输入验证码");
+                    return;
+                }
+                if (!isClickCode) {
+                    mDataManager.showToast("请点击获取验证码");
                     return;
                 }
                 gotoActivity(TabHomeAct.class);
@@ -129,5 +178,12 @@ public class LoginAct extends BaseAppActivity {
                 gotoActivity(AuthAct.class);
                 break;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (timer != null)
+            timer.cancel();
     }
 }
